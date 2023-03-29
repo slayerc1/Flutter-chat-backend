a391dd135c478dcb9bd96002b2233271bf99a292
Raul Rodriguez Olmo <r.rodriguez-olmo@wata.es>
2023-03-29 13:37:40 +0200

    Fin sección 8

---
+++ database/config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async() => {
+    try {
+        await mongoose.connect(process.env.DB_CNN);
+
+        console.log('DB Online');
+    } catch (error) {
+        console.log(error);
+        throw new Error('Error en la base de datos - Hable con el administrador');
+    }
+}
+
+module.exports = {
+    dbConnection
+}
